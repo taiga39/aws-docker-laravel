ad1e6519b0bfecbbe3b311195e6c76b53257dd1f
taiga39 <ishikawa.tigers@gmail.com>
2020-09-16 01:40:20 +0900

    テストget 赤

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', 'HelloController@index');
+Route::get('/tasks', 'TaskController@index');
---
+++ src/tests/Feature/TaskControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAllTaskPath(){
+        $response = $this->get('/tasks');
+
+        $response->assertStatus(200);
+    }
+}
